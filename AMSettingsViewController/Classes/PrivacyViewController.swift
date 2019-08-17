2afe074f8d7c1256ca182f01490d5bcef55a839e
Anumothu <anumothu@hakunamatata.in>
2019-08-18 01:12:33 +0530

    All dynamic proprty has been added

---
+++ AMSettingsViewController/Classes/PrivacyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PrivacyViewController.swift
+//  AMSettingsViewController
+//
+//  Created by Mac on 18/08/19.
+//
+
+import UIKit
+
+class PrivacyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
